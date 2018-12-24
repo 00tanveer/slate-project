daebaa63727c239381641eb2741db5e0f03e5eee
00tanveer <tanveer_fh09@yahoo.com>
2018-12-25 01:48:30 +0600

    moved Button and Icon components to toolbar directory

---
+++ src/components/toolbar/Icon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Icon = styled(({ className, ...rest }) => {
+  return <span className={`material-icons ${className}`} {...rest} />
+})`
+  font-size: 18px;
+  vertical-align: text-bottom;
+`
